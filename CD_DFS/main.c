d28ea023b8628d968f842fb99f8cbcd00b163c9e
Reegan Suman <145418786+sumanzzz@users.noreply.github.com>
2025-04-07 03:57:12 +0530

    Update dfs

--- CD_DFS/main.c
+++ CD_DFS/main.c
@@ -14,7 +14,7 @@
 //visits recursively all the unvisited vertices connected to vertex v by a path and numbers them in the order they are encountered via global variable count
 /*
     count <- count +1;
-    for each vertex w in v adjacent to v do
+    for each vertex w in V adjacent to v do
         if w is marked with 0
             dfs(w)
 
